8da5210963e10699267f432671748c3fff35d9cb
Yomaira Escano <yjescano@gmail.com>
2018-02-04 02:37:26 +0000

    automatically backed up by learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,11 +1,11 @@
 class Genre < ActiveRecord::Base
   extend Concerns::Slugifiable
-  
+
   has_many :artists, through: :songs
   has_many :song_genres
   has_many :songs, through: :song_genres
 
-  def slug
-    self.name.downcase.gsub(" ", "-")
-  end
+  # def slug
+  #   self.name.downcase.gsub(" ", "-")
+  # end
 end
