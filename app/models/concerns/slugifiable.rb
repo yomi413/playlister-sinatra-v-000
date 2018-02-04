0a4f0ee466cb1919ff9658945b689f5b93ace0e6
Yomaira Escano <yjescano@gmail.com>
2018-02-04 02:36:59 +0000

    automatically backed up by learn

--- app/models/concerns/slugifiable.rb
+++ app/models/concerns/slugifiable.rb
@@ -1,5 +1,11 @@
 module Concerns::Slugifiable
 
+  def slug
+    Artist.name.downcase.gsub(" ", "-")
+    Song.name.downcase.gsub(" ", "-")
+    Genre.name.downcase.gsub(" ", "-")
+  end
+
   def find_by_slug(slug)
     self.all.detect{|variable| variable.slug == slug}
   end
