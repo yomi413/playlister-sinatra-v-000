729b1c9630386e18a2bc80dd15e98a45c683b035
Yomaira Escano <yjescano@gmail.com>
2018-02-04 02:37:23 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,11 +1,11 @@
 class Song < ActiveRecord::Base
   extend Concerns::Slugifiable
-  
+
   belongs_to :artist
   has_many :song_genres
   has_many :genres, through: :song_genres
 
-  def slug
-    self.name.downcase.gsub(" ", "-")
-  end
+  # def slug
+  #   self.name.downcase.gsub(" ", "-")
+  # end
 end
